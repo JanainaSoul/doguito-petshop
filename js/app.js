cc46a7ad2676fb1148e97e4d8a1e4b4c0b11edfe
Janaina <janainasoul@icloud.com>
2023-03-07 17:25:33 -0300

    validando campo de data com javascript

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import {valida} from './novaValidacao.js';
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    input.addEventListener('blur', (evento) =>{
+        valida(evento.target);
+    })
+})
